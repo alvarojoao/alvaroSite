366faa48355d31a62fdfab94767398aee2846713
alvarojoao <alvaro@silvino.me>
2015-09-02 20:15:24 -0300

    first commit on project

---
+++ app/controllers/Application.scala
@@ -0,0 +1,17 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.i18n.I18nSupport
+import play.api.i18n.Messages.Implicits._
+import play.api.i18n.MessagesApi
+import javax.inject.Inject
+import play.api.i18n._
+class Application  @Inject() ( val messagesApi: MessagesApi) extends Controller with I18nSupport {
+
+  def index = Action { request =>
+
+    Ok(views.html.index())
+  }
+
+}
