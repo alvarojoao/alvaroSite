ad32adfb20b75c482abd4f87445502e2af92a4db
alvarojoao <alvaro@silvino.me>
2015-09-03 14:08:01 -0300

    final fixing on multilanguage suport

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,14 +6,15 @@ import play.api.i18n.I18nSupport
 import play.api.i18n.Messages.Implicits._
 import play.api.i18n.MessagesApi
 import javax.inject.Inject
+import play.api.i18n.Lang
 import play.api.i18n._
 class Application  @Inject() ( val messagesApi: MessagesApi) extends Controller with I18nSupport {
 
-
-
-
 	def index = Action {  implicit request =>
-		Ok(views.html.index())
+				request.getQueryString("lang") match{
+			case Some(lang) =>  messagesApi.setLang(Ok(views.html.index()(messagesApi,Lang(lang))),Lang(lang))
+			case None => messagesApi.setLang(Ok(views.html.index()(messagesApi,Lang("en"))),Lang("en"))
+		}
 	}
 
 }
